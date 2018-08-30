bd0193ef9c565a4ee76ae1432b129ce599102ddb
Andrew-Hogan <andrewmichaelhogan94@gmail.com>
2018-08-29 19:11:33 -0500

    Subclassing works - just want it to work smoother with super()
    almost to acceptable general functionality

--- map_keys.py
+++ map_keys.py
@@ -35,17 +35,7 @@ def _safe_self_access_decorator(wrapped_method):
     return method_wrapper
 
 
-def _borg_pod_set_with_safe_self_access(wrapped_method):
-    @functools.wraps(wrapped_method)
-    def setter_wrapper(self, attribute, value):
-        if _should_protect_self_access(attribute, value):
-            print("WRAPPING IT: {}".format(attribute))
-            value = _safe_self_access_decorator(value)
-        super(self.__class__, self).__setattr__(attribute, value)
-    return setter_wrapper
-
-
-def assimilate(_wrapped_class=None, *, default_class=None):
+def assimilate(_wrapped_class=None, *, default_class=None, _directly_wrapped_classes=set(), _from_sub_canary=[]):
     """
 
     Assumptions:
@@ -62,83 +52,111 @@ def assimilate(_wrapped_class=None, *, default_class=None):
     if default_class is None:
         default_class = ShapeDescriptor
 
-    this_class_wrapped_canary = []
-
     def borg_pod_decorator(wrapped_class):
-        @functools.wraps(wrapped_class)
-        def pod_wrapper(*c_args, **c_kwargs):
-            def _setup_pod_in_new(wrapped_new):
-                @functools.wraps(wrapped_new)
-                def new_wrapper(cls, *args, queen=None, _base_class=None, **kwargs):
-                    if _base_class is None:
-                        _base_class = default_class
-                    if queen is None:
-                        for ids, arg in enumerate(args):
-                            if isinstance(arg, _base_class):
-                                queen = arg
-                                args = args[:ids] + args[ids + 1:]
-                                break
-                        else:
-                            queen = _base_class({})
-                    shared_state = queen.__dict__
+        def _setup_pod_in_new(wrapped_new):
+            @functools.wraps(wrapped_new)
+            def new_wrapper(cls, *args, queen=None, _base_class=None, **kwargs):
+                if _from_sub_canary:
+                    print("FROM SUB NEW CALLED")
+                    new = wrapped_new(cls)
+                    return [new]
+                print("NEW CALLED")
+                if _base_class is None:
+                    _base_class = default_class
+                is_wrapped_subclass = True
+                if not any((pappy in _directly_wrapped_classes for pappy in ancestors)):
+                    if _should_be_self_unless_multi_wrapped == cls:
+                        is_wrapped_subclass = False
+                if queen is None:
+                    for ids, arg in enumerate(args):
+                        if isinstance(arg, _base_class):
+                            queen = arg
+                            args = args[:ids] + args[ids + 1:]
+                            break
+                    else:
+                        queen = _base_class({})
+
+                shared_state = queen.__dict__
+                if is_wrapped_subclass:
+                    _from_sub_canary.append(None)
+                    new_object = wrapped_new(cls).pop()
+                    del _from_sub_canary[-1]
+                    print("FROM SUB NEW RECEIVED")
+                else:
                     new_object = wrapped_new(cls)
-                    new_object.__init__(shared_state, *args, **kwargs)
-                    return queen
-                return new_wrapper
+                new_object.__init__(shared_state, *args, **kwargs)
+                if is_wrapped_subclass:
+                    del _from_sub_canary[-1]
+                    print("SUB INIT FINISHED")
+                return queen
+            return new_wrapper
 
-            def _assimilate_in_init(wrapped_init):
-                @functools.wraps(wrapped_init)
-                def init_wrapper(self, shared_state, *args, **kwargs):
-                    self.__dict__ = shared_state
-                    self._active_descriptor = self
-                    self.queen = self._protected_self.queen
-                    self.drone = self._protected_self.drone
-                    return wrapped_init(self, *args, **kwargs)
-                return init_wrapper
+        def _assimilate_in_init(wrapped_init):
+            @functools.wraps(wrapped_init)
+            def init_wrapper(self, shared_state, *args, **kwargs):
+                if any((pappy in _directly_wrapped_classes for pappy in ancestors)):
+                    if _from_sub_canary:
+                        return wrapped_init(self, shared_state, *args, **kwargs)
+                    else:
+                        _from_sub_canary.append(None)
+                elif _from_sub_canary:
+                    return ancestors[0].__init__(self, *args, **kwargs)
+                self.__dict__ = shared_state
+                self._active_descriptor = self
+                self.queen = self._protected_self.queen
+                self.drone = self._protected_self.drone
+                if _from_sub_canary:
+                    return wrapped_init(self, shared_state, *args, **kwargs)
+                return wrapped_init(self, *args, **kwargs)
+            return init_wrapper
 
-            def _modify_methods_for_self_reference(this_class):
-                for c_attribute, c_method in this_class.__dict__.copy().items():
-                    if _should_protect_self_access(c_attribute, c_method):
-                        print("{} wrapped by method dec.".format(c_attribute))
-                        setattr(this_class, c_attribute, _safe_self_access_decorator(this_class.__dict__[c_attribute]))
-                        c_method._protect_self_reference = False
+        def _modify_methods_for_self_reference(this_class):
+            for c_attribute, c_method in this_class.__dict__.copy().items():
+                if _should_protect_self_access(c_attribute, c_method):
+                    print("{} wrapped by method dec.".format(c_attribute))
+                    setattr(this_class, c_attribute, _safe_self_access_decorator(this_class.__dict__[c_attribute]))
+                    c_method._protect_self_reference = False
 
-            if this_class_wrapped_canary:
-                return wrapped_class(*c_args, **c_kwargs)
+        def _borg_pod_set_with_safe_self_access(wrapped_method):
+            @functools.wraps(wrapped_method)
+            def setter_wrapper(self, attribute, value):
+                if _should_protect_self_access(attribute, value):
+                    print("WRAPPING IT: {}".format(attribute))
+                    value = _safe_self_access_decorator(value)
+                for super_class in _all_ancestors:
+                    modified_setter = hasattr(super(super_class, self).__setattr__, "_protect_self_reference")
+                    if not modified_setter:
+                        super(super_class, self).__setattr__(attribute, value)
+                        break
+            return setter_wrapper
 
-            # Instance method self-reference-return protector
-            _modify_methods_for_self_reference(wrapped_class)
+        _directly_wrapped_classes.update({wrapped_class})
+        _all_ancestors = wrapped_class.mro()
+        _should_be_self_unless_multi_wrapped, ancestors = _all_ancestors[0], _all_ancestors[1:]
+        print("WRAPPED CLASSES: {}".format(_directly_wrapped_classes))
+        print(ancestors)
 
-            setattr(wrapped_class, '__new__', _setup_pod_in_new(wrapped_class.__new__))  # Check for pre-set?
-            wrapped_class.__new__._protect_self_reference = False
-            setattr(wrapped_class, '__init__', _assimilate_in_init(wrapped_class.__init__))
-            wrapped_class.__init__._protect_self_reference = False
-            setattr(wrapped_class, '__hash__', lambda x: hash(x.queen))
-            wrapped_class.__hash__._protect_self_reference = False
-            setattr(wrapped_class, '__eq__', lambda x, y: x.queen is y.queen if hasattr(y, "queen") else False)
-            wrapped_class.__eq__._protect_self_reference = False
-            setattr(wrapped_class, '__setattr__', _borg_pod_set_with_safe_self_access(wrapped_class.__setattr__))
-            wrapped_class.__setattr__._protect_self_reference = False
+        # Instance method self-reference-return protector
+        _modify_methods_for_self_reference(wrapped_class)
 
-            ancestors = wrapped_class.mro()
-            for ancestor in ancestors[1:]:
-                print(ancestor)
-                if ancestor is not object:
-                    _modify_methods_for_self_reference(ancestor)
+        setattr(wrapped_class, '__new__', _setup_pod_in_new(wrapped_class.__new__))  # Check for pre-set?
+        wrapped_class.__new__._protect_self_reference = False
+        setattr(wrapped_class, '__init__', _assimilate_in_init(wrapped_class.__init__))
+        wrapped_class.__init__._protect_self_reference = False
+        setattr(wrapped_class, '__hash__', lambda x: hash(x.queen))
+        wrapped_class.__hash__._protect_self_reference = False
+        setattr(wrapped_class, '__eq__', lambda x, y: x.queen is y.queen if hasattr(y, "queen") else False)
+        wrapped_class.__eq__._protect_self_reference = False
+        setattr(wrapped_class, '__setattr__', _borg_pod_set_with_safe_self_access(wrapped_class.__setattr__))
+        wrapped_class.__setattr__._protect_self_reference = False
 
-            this_class_wrapped_canary.append(None)
-            print("A")
-            print(wrapped_class)
-            print(type(wrapped_class))
-            return wrapped_class(*c_args, **c_kwargs)
-        print("B")
-        print(pod_wrapper)
-        print(type(pod_wrapper))
-        return pod_wrapper
+        for ancestor in ancestors:
+            print(ancestor)
+            if ancestor is not object:
+                _modify_methods_for_self_reference(ancestor)
 
-    print("C")
-    print(borg_pod_decorator)
-    print(type(borg_pod_decorator))
+        return wrapped_class
+
     if _wrapped_class is None:
         return borg_pod_decorator
     return borg_pod_decorator(_wrapped_class)
@@ -184,18 +202,31 @@ class ShapeDescriptor(object):
         return hash(self) == hash(other)
 
 
-class PerfectGreekInfluencedChalkDrawingOfFace(object):
-    def __init__(self):
+class Useless(object):
+    def __init__(self, *args, **kwargs):
+        print(args)
+        print(kwargs)
+
+
+class PerfectGreekInfluencedChalkDrawingOfFace(Useless):
+    def __init__(self, *args, **kwargs):
+        print(self.__class__)
+        super(self.__class__, self).__init__(*args, **kwargs)
         self.shape_type = "pre-circle"
 
     def self_method(self):
         return self
 
 
-@assimilate
+print("C_SPONGE")
+print(PerfectGreekInfluencedChalkDrawingOfFace)
+print(type(PerfectGreekInfluencedChalkDrawingOfFace))
+
+
+@assimilate(default_class=ShapeDescriptor)
 class Circle(PerfectGreekInfluencedChalkDrawingOfFace):
-    def __init__(self):
-        super(self.__class__, self).__init__()
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
         self.shape_type = "circle"
 
     @staticmethod
@@ -215,15 +246,22 @@ class Circle(PerfectGreekInfluencedChalkDrawingOfFace):
     __repr__ = __str__
 
 
-print("D")
+print("C")
 print(Circle)
 print(type(Circle))
 
 
-class Ellipse(object):
-    def __init__(self):
-        super(self.__class__, self).__init__()
+@assimilate
+class Ellipse(Circle):
+    def __init__(self, *args, **kwargs):
+        print("ELLIPSE CLASS INIT CALLED")
+        super(self.__class__, self).__init__(*args, **kwargs)
         self.shape_type = "ellipse"
+
+
+print("C-sponge")
+print(Ellipse)
+print(type(Ellipse))
 
 
 @assimilate
@@ -249,6 +287,11 @@ class AlphaNumeric(object):
     __repr__ = __str__
 
 
+print("C-Alpha")
+print(AlphaNumeric)
+print(type(AlphaNumeric))
+
+
 @assimilate
 class Punctuation(object):
     def __init__(self):
@@ -270,6 +313,11 @@ class Punctuation(object):
         return "<Unassimilated {} object #{}>".format(self.__class__.__name__, id(self))
 
     __repr__ = __str__
+
+
+print("C-Punct")
+print(Punctuation)
+print(type(Punctuation))
 
 
 def compare_seq(sequence, sequence_2=None):
@@ -373,10 +421,14 @@ def main(num_objects=6):
     print(self_list_ambig)
     print("Are they equal to the old version?")
     compare_seq(self_list_ambig, test_objects_circle)
-    print("What if we use the subclass?")
-    test_objects_undecorated_subclass = [PerfectGreekInfluencedChalkDrawingOfFace() for _ in range(num_objects)]
-    self_list_face = [obj.self_method() for obj in test_objects_undecorated_subclass]
+    print("What if we use instances of a parent class?")
+    test_objects_undecorated_parent_class = [PerfectGreekInfluencedChalkDrawingOfFace() for _ in range(num_objects)]
+    self_list_face = [obj.self_method() for obj in test_objects_undecorated_parent_class]
     print(self_list_face)
+    print("What if we use a child class?")
+    test_objects_decorated_subclass = [Ellipse() for _ in range(num_objects)]
+    self_list_sub = [obj.self_method() for obj in test_objects_decorated_subclass]
+    print(self_list_sub)
 
     print("\nTo Characters-")
     test_objects_characters = convert_seq(test_objects_circle, AlphaNumeric)
